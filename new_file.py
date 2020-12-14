940bc624abb7725dd19fc79b821876e2df702c5d
Bacmel <hugo.duarte91800@gmail.com>
2020-12-14 17:47:13 +0100

    init Partie 2

---
+++ new_file.py
@@ -0,0 +1,18 @@
+def algo_Q_7(i, j, Y):
+    if i == 0 or j == 0 :
+        return np.array([0, 0]);
+    else:
+        
+        T = np.concatenate(
+        return tri_fusion()
+
+def algo_Q_8(y, I):
+    if y[0]>y[1]:
+        alpha = I[1]
+    else:
+        alpha = I[0]
+    return (y[0]-y[1])*alpha + y[1]
+
+def algo_Q_9(T):
+    
+    return (y[0]-y[1])*alpha + y[1]
